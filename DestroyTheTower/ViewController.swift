886d1757898c32ffbbb541c81435260f1939d51d
brett-sillitoe <bsilly1229@gmail.com>
2020-04-15 14:52:37 -0600

    Initial Commit

---
+++ DestroyTheTower/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DestroyTheTower
+//
+//  Created by Brett Sillitoe on 4/15/20.
+//  Copyright © 2020 Brett Sillitoe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
